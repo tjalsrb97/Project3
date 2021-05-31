201c60776bbb45d38945b1737dcd46ca093cc5e8
tjalsrb97 <gnam011@naver.com>
2021-05-31 22:15:37 +0900

    바꿨다고 ,추가

--- routes/customer.js
+++ routes/customer.js
@@ -310,7 +310,7 @@ router.get('/detail/:PID/review', function (req, res) {
       res.render('write_review', {
         title: "리뷰 작성",
         product_deal: rows[rows.length - 1],
-        user_id: user_id
+        user_id: user_id,
         name: req.session.user.name
       });
       connection.release();
