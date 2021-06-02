1c0b166aeccc3cdfa4bebe759803e0979f13eab3
Kyankee6 <didzl1231@naver.com>
2021-06-02 18:39:35 +0900

    로그인, 회원가입 폼 디자인 추가

--- routes/logout.js
+++ routes/logout.js
@@ -6,8 +6,10 @@ var crypto = require('crypto');
 
 
 router.get('/', function(req, res) {
+    console.log(req.session.user);
     delete req.session.user;
     req.session.save(() => {
+        console.log(req.session);
         res.redirect('/');
     });
 });
